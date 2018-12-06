c85dba3eb191c4a65502ed191d6ca34da002d54c
Fabian <fabian.deroose@gmail.com>
2018-12-06 12:46:09 +0100

    Correction effet pacman

--- mariov2_fabian.py
+++ mariov2_fabian.py
@@ -32,9 +32,9 @@ pygame.display.set_caption('MARIO')
 liste_ennemi = []
 
 #---INITIALISER MARIO---#
-mario_enSaut = False
-mario_x, mario_y = FENETRE_LARGEUR//2, FENETRE_HAUTEUR - 100
-mario_vx, mario_vy = 0, 0
+#mario_enSaut = False
+#mario_x, mario_y = FENETRE_LARGEUR//2, FENETRE_HAUTEUR - 100
+#mario_vx, mario_vy = 0, 0
 
 #---CHARGEMENT IMAGES---#
 background = pygame.image.load('images/background2.png')
@@ -42,13 +42,11 @@ plateforme = pygame.image.load('images/platform2.png')
 plateforme = pygame.transform.scale(plateforme, (25, 25))
 sol = pygame.image.load('images/brick2.png')
 sol = pygame.transform.scale(sol, (25, 25))
-mario_image = pygame.Surface((25, 25))
-mario_image.fill(ROUGE)
 ennemi_image = pygame.Surface((25, 25))
 ennemi_image.fill(VERT)
 
-###DEFINITION ENTITES###
-class entite():
+###DEFINITION classe_ennemiS###
+class classe_ennemi():
     def __init__(self, pos_x, pos_y):
         self.x = pos_x
         self.y = pos_y
@@ -62,7 +60,7 @@ class entite():
         self.x += self.vx
         self.y += self.vy
         self.x, self.y, self.vx, self.vy = bloque_sur_collision(map, (self.ancien_x, self.ancien_y), (self.x, self.y), self.vx, self.vy)
-        ###from_coord_to_grid###
+        ###coordonnees_vers_grille###
         self.x, self.y = self.pos
         self.i = max(0, int(self.x // 25))
         self.j = max(0, int(self.y // 25))
@@ -87,6 +85,70 @@ class entite():
                 self.x = 10
                 self.y = 125
             else: self.x = 10
+
+class joueur_principal():
+    def __init__(self, x, y, largeur, hauteur):
+        self.hauteur = hauteur
+        self.largeur = largeur
+        self.x = x
+        self.y = y
+        self.vx = 0
+        self.vy = 0
+        self.enSaut = False
+        self.droite = False
+        self.gauche = False
+        self.compteurImage = 0
+        # IMAGES
+        self.mario0d = pygame.image.load('images/mario0d.png')
+        self.mario0d = pygame.transform.scale(self.mario0d, (self.largeur, self.hauteur))
+        self.mario1d = pygame.image.load('images/mario1d.png')
+        self.mario1d = pygame.transform.scale(self.mario1d, (self.largeur, self.hauteur))
+        self.mario2d = pygame.image.load('images/mario2d.png')
+        self.mario2d = pygame.transform.scale(self.mario2d, (self.largeur, self.hauteur))
+        self.mario3d = pygame.image.load('images/mario3d.png')
+        self.mario3d = pygame.transform.scale(self.mario3d, (self.largeur, self.hauteur))
+        self.mariod = [self.mario0d, self.mario1d, self.mario0d, self.mario1d, self.mario0d, self.mario1d, self.mario0d, self.mario1d, self.mario0d, self.mario1d, self.mario0d, self.mario1d, self.mario0d, self.mario1d, self.mario0d, self.mario1d]
+        self.mario0g = pygame.image.load('images/mario0g.png')
+        self.mario0g = pygame.transform.scale(self.mario0g, (self.largeur, self.hauteur))
+        self.mario1g = pygame.image.load('images/mario1g.png')
+        self.mario1g = pygame.transform.scale(self.mario1g, (self.largeur, self.hauteur))
+        self.mario2g = pygame.image.load('images/mario2g.png')
+        self.mario2g = pygame.transform.scale(self.mario2g, (self.largeur, self.hauteur))
+        self.mario3g = pygame.image.load('images/mario3g.png')
+        self.mario3g = pygame.transform.scale(self.mario3g, (self.largeur, self.hauteur))
+        self.mariog = [self.mario0g, self.mario1g, self.mario0g, self.mario1g, self.mario0g, self.mario1g, self.mario0g, self.mario1g, self.mario0g, self.mario1g, self.mario0g, self.mario1g, self.mario0g, self.mario1g, self.mario0g, self.mario1g]
+        self.mariosg = pygame.image.load('images/mariosg.png')
+        self.mariosg = pygame.transform.scale(self.mariosg, (self.largeur, self.hauteur))
+        self.mariosd = pygame.image.load('images/mariosd.png')
+        self.mariosd = pygame.transform.scale(self.mariosd, (self.largeur, self.hauteur))
+    def affiche_joueur_principal(self, fenetre):
+        if self.compteurImage + 1 >= 32:
+            self.compteurImage = 0
+        if self.gauche and not self.enSaut:
+            fenetre.blit(self.mariog[self.compteurImage // 2], (self.x, self.y))
+            self.compteurImage += 1
+        elif self.droite and not self.enSaut:
+            fenetre.blit(self.mariod[self.compteurImage // 2], (self.x, self.y))
+            self.compteurImage += 1
+        elif self.droite and self.enSaut:
+            fenetre.blit(self.mariosd, (self.x, self.y))
+            self.compteurImage += 1
+        elif self.gauche and self.enSaut:
+            fenetre.blit(self.mariosg, (self.x, self.y))
+            self.compteurImage += 1
+        elif not self.gauche and not self.droite and self.enSaut:
+            fenetre.blit(self.mariosd, (self.x, self.y))
+        else: fenetre.blit(self.mario0d, (self.x, self.y))
+    def mise_a_jour_position(self):
+        self.ancien_x, self.ancien_y = self.x, self.y
+        self.vy += GRAVITE
+        self.x += self.vx
+        self.y += self.vy
+        self.x, self.y, self.vx, self.vy = bloque_sur_collision(map, (self.ancien_x, self.ancien_y), (self.x, self.y), self.vx, self.vy)
+        if self.x <= 10:
+            self.x = FENETRE_LARGEUR - 35
+        elif self.x >= FENETRE_LARGEUR - 35:
+            self.x = 10
 
 #---CONFIG MAP---#
 map= [    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -121,24 +183,32 @@ def diagnostics():
         print("Delai apparition monstres :", temps_spawn)
 
 def traite_entrees():
-    global mario_x, mario_y, mario_vx, mario_vy, mario_enSaut, fini, score, temps_spawn
+    global fini, score, temps_spawn
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             fini = True
-        elif event.type == KEYDOWN:
-            if event.key == K_SPACE and mario_enSaut == False:
-                mario_vy = -35
-                mario_enSaut = True
+        elif event.type == nouveauennemi:
+            generer_ennemi()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and mario.enSaut == False:
+                mario.vy = -35
+                mario.enSaut = True
             if event.key == pygame.K_g:
                 generer_ennemi()
                 score += 100
                 temps_spawn += 25
-        elif event.type == nouveauennemi:
-            generer_ennemi()
-        elif mario_vy == 0:
-            mario_enSaut = False
-    keys_pressed = pygame.key.get_pressed()
-    mario_vx = (keys_pressed[K_RIGHT] - keys_pressed[K_LEFT]) * 5
+    touche_maintenue = pygame.key.get_pressed()
+    if touche_maintenue[pygame.K_RIGHT]:
+        mario.droite = True
+        mario.gauche = False
+    if touche_maintenue[pygame.K_LEFT]:
+        mario.gauche = True
+        mario.droite = False
+    if mario.vy == 0:
+        mario.enSaut = False
+    if mario.vx == 0:
+        mario.compteurImage = 0
+    mario.vx = (touche_maintenue[pygame.K_RIGHT] - touche_maintenue[pygame.K_LEFT]) * 5
 
 def dessiner_map(fenetre, map):
     for j, ligne in enumerate(map):
@@ -148,13 +218,13 @@ def dessiner_map(fenetre, map):
             elif case == 2:
                 fenetre.blit(sol, (i*25, j*25))
 
-def from_coord_to_grid(mario_pos):
+def coordonnees_vers_grille(mario_pos):
     mario_x, mario_y = mario_pos
     i = max(0, int(mario_x // 25))
     j = max(0, int(mario_y // 25))
     return i, j
 
-def get_neighbour_blocks(map, i_start, j_start):
+def recuperer_blocs_voisins(map, i_start, j_start):
     global i
     blocks = []
     for j in range(j_start, j_start+2):
@@ -167,13 +237,13 @@ def get_neighbour_blocks(map, i_start, j_start):
 def bloque_sur_collision(map, ancien_pos, nouv_pos, mario_vx, mario_vy):
     ancien_rect = pygame.Rect(ancien_pos, (25, 25))
     nouv_rect = pygame.Rect(nouv_pos, (25, 25))
-    i, j = from_coord_to_grid(nouv_pos)
-    collide_later = []
-    blocks = get_neighbour_blocks(map, i, j)
+    i, j = coordonnees_vers_grille(nouv_pos)
+    collision_future = []
+    blocks = recuperer_blocs_voisins(map, i, j)
     for block in blocks:
         if not nouv_rect.colliderect(block):
             continue
-        dx_correction, dy_correction = compute_penetration(block, ancien_rect, nouv_rect)
+        dx_correction, dy_correction = calculer_penetration(block, ancien_rect, nouv_rect)
         if dx_correction == 0.0:
             nouv_rect.top += dy_correction
             mario_vy = 0.0
@@ -181,9 +251,9 @@ def bloque_sur_collision(map, ancien_pos, nouv_pos, mario_vx, mario_vy):
             nouv_rect.left += dx_correction
             mario_vx = 0.0
         else:
-            collide_later.append(block)
-    for block in collide_later:
-        dx_correction, dy_correction = compute_penetration(block, ancien_rect, nouv_rect)
+            collision_future.append(block)
+    for block in collision_future:
+        dx_correction, dy_correction = calculer_penetration(block, ancien_rect, nouv_rect)
         if dx_correction == dy_correction == 0.0:
             continue
         if abs(dx_correction) < abs(dy_correction):
@@ -199,7 +269,7 @@ def bloque_sur_collision(map, ancien_pos, nouv_pos, mario_vx, mario_vy):
     mario_x, mario_y = nouv_rect.topleft
     return mario_x, mario_y, mario_vx, mario_vy
 
-def compute_penetration(block, ancien_rect, nouv_rect):
+def calculer_penetration(block, ancien_rect, nouv_rect):
     dx_correction = dy_correction = 0.0
     if ancien_rect.bottom <= block.top < nouv_rect.bottom:
         dy_correction = block.top  - nouv_rect.bottom
@@ -211,28 +281,23 @@ def compute_penetration(block, ancien_rect, nouv_rect):
         dx_correction = block.right - nouv_rect.left
     return dx_correction, dy_correction
 
-def mise_a_jour_position_mario():
-    global mario_x, mario_y, mario_vx, mario_vy, GRAVITE, map
-    ancien_x, ancien_y = mario_x, mario_y
-    mario_vy += GRAVITE
-    mario_x += mario_vx
-    mario_y += mario_vy
-    mario_x, mario_y, mario_vx, mario_vy = bloque_sur_collision(map, (ancien_x, ancien_y), (mario_x, mario_y), mario_vx, mario_vy)
-    if mario_x == 0:
-        mario_x = FENETRE_LARGEUR - 35
-    elif mario_x == FENETRE_LARGEUR - 30:
-        mario_x = 5
-
 def generer_ennemi():
     global temps_spawn
-    ennemi = entite(random.randint(25, FENETRE_LARGEUR - 25), 25)
+    ennemi = classe_ennemi(random.randint(25, FENETRE_LARGEUR - 25), 25)
     liste_ennemi.append(ennemi)
     temps_spawn -= 50
     pygame.time.set_timer(nouveauennemi, temps_spawn)
-def affichage_ennemi():
-    for ennemi in liste_ennemi:
-        ennemi.mise_a_jour_position_ennemi()
-        fenetre.blit(ennemi_image, (ennemi.x, ennemi.y))
+
+def affichage_acteurs():
+    def affichage_ennemi():
+        for ennemi in liste_ennemi:
+            ennemi.mise_a_jour_position_ennemi()
+            fenetre.blit(ennemi_image, (ennemi.x, ennemi.y))
+    def affichage_mario():
+        mario.mise_a_jour_position()
+        mario.affiche_joueur_principal(fenetre)
+    affichage_mario()
+    affichage_ennemi()
 
 def dessiner_infos():
     global affichage_score, affichage_score2, affichage_temps, temps_spawn
@@ -252,15 +317,14 @@ def dessiner_infos():
 
 #--- BOUCLE PRINCIPALE ---#
 pygame.init()
+mario = joueur_principal(25, 80, 25, 25)
 while not fini:
     temps_ecoule = time.clock()
     traite_entrees()
     fenetre.blit(background, (0, 0))
     dessiner_map(fenetre, map)
     dessiner_infos()
-    mise_a_jour_position_mario()
-    fenetre.blit(mario_image, (mario_x, mario_y))
-    affichage_ennemi()
+    affichage_acteurs()
     horloge.tick(30)
     pygame.display.flip()
     diagnostics()
